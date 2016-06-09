6c5c27a26e9b0cbf60e88970b78370f5e37b01d2
Steve Hwang <shwang@skyhookwireless.com>
2016-06-09 12:57:04 -0400

    Bluetooth data fixes

--- app/src/main/java/com/skyhookwireless/venuelock/BlankFragment.java
+++ app/src/main/java/com/skyhookwireless/venuelock/BlankFragment.java
@@ -346,7 +346,6 @@ public class BlankFragment extends Fragment {
                         break;
                 }
             }
-
             return null;
         }
 
--- app/src/main/java/com/skyhookwireless/venuelock/DataBaseHelper.java
+++ app/src/main/java/com/skyhookwireless/venuelock/DataBaseHelper.java
@@ -130,7 +130,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
     {
         if (newVersion > oldVersion)
         {
-            Log.v("VenueLock Database Upgrade", "Database version higher than old.");
+            Log.v("VenueLock Database", "Database version higher than old.");
             db_delete();
         }
     }
@@ -145,7 +145,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
     public String getVidForMac(String mac) {
         String s = "";
         try {
-            Log.d("VenueLock Database Helper", "getVidForMac");
+            Log.d("VenueLock Database", "getVidForMac");
             String getVidQuery = "select vid from venue_mac_philly where _id=\"" + mac + "\"" ;
             SQLiteDatabase db = this.getReadableDatabase();
             Cursor cursor = db.rawQuery(getVidQuery, null);
@@ -166,7 +166,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
 
     public ScannedVenue getScannedVenue(String mac) {
         try {
-            Log.d("VenueLock Database Helper", "getScannedVenue");
+            Log.d("VenueLock Database", "getScannedVenue");
             String getVidQuery = "select vid, vname, vlatitude, vlongitude from venue_mac_philly where _id=\"" + mac + "\"" ;
             SQLiteDatabase db = this.getReadableDatabase();
             Cursor cursor = db.rawQuery(getVidQuery, null);
